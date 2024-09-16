69f08692ce48e184b457e823e8fa400232a9f9d7
asaphoncho <uniqueeph38@gmail.com>
2024-09-16 23:26:36 +0100

    new update

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -26,35 +26,35 @@ public_users.post("/register", (req,res) => {
 });
 
 // Get the book list available in the shop
-/*public_users.get('/',function (req, res) {
+public_users.get('/',function (req, res) {
   res.send(JSON.stringify(books, null, 4))
-});*/
+});
 
 // Get book details based on ISBN
-/*public_users.get('/isbn/:isbn',function (req, res) {
+public_users.get('/isbn/:isbn',function (req, res) {
   const ISBN = req.params.isbn
   const searched_book = Object.values(books).find((book) => book.ISBN === ISBN)
 
   res.send(searched_book)
- });*/
+ });
   
 // Get book details based on author
-/*public_users.get('/author/:author',function (req, res) {
+public_users.get('/author/:author',function (req, res) {
   const author = req.params.author
   const searched_book = Object.values(books).find((book) => book.author.toLowerCase() == author.toLowerCase())
 
   res.send(searched_book)
   //Write your code here
   return res.status(300).json({message: "Yet to be implemented"});
-});*/
+});
 
 // Get all books based on title
-/*public_users.get('/title/:title',function (req, res) {
+public_users.get('/title/:title',function (req, res) {
   const title = req.params.title
   const searched_book = Object.values(books).find((book) => book.title == title)
 
   res.send(searched_book)
-});*/
+});
 
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
